61ad4bf2cb934ec270307601a269a5743ac514aa
kinngddx <ddxboss70@gmail.com>
2026-01-26 19:35:01 +0530

    fix build error

--- app/api/cases/route.ts
+++ app/api/cases/route.ts
@@ -1,5 +1,7 @@
 
 // import { Prisma } from '@prisma/client';
+export const dynamic = "force-dynamic";
+
 
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
